0b178f17d8a882ed5f7922171eca2c3593016e2b
Anirban166 <bloodraven166@gmail.com>
2019-05-24 22:35:35 +0530

    This pointer implementation for objects(age,name).

---
+++ Thispointer.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string.h> 
+#include<cstring>
+using namespace std;
+class person
+{ int age; char name[20];
+	public:
+	person(int age=0, char *n="name")
+	{	
+	this->age=age;
+        strcpy(name,n);
+	}
+	void disp()
+	{ cout<<"youngest age: "<<age<<"and name:"<<name;
+	}
+	person &youngest(person &x)
+	{ if(x.age<age)
+	  return x;
+	  else
+	  return *this;
+	}
+};
+int main()
+{ person a(14,"Stark"), b(15,"Jarvis"), c;
+  c=a.youngest(b);
+  c.disp();
+  return 0; 
+}
