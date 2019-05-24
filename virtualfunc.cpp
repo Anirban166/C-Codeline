0707d828db009219003ec5e43abd6d1be84db91f
Anirban166 <bloodraven166@gmail.com>
2019-05-24 22:09:46 +0530

    Virtual Function implementation.

---
+++ virtualfunc.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Shape                   
+{
+    public:
+   
+       virtual void calculateArea() = 0;
+};
+
+class Square : public Shape
+{
+    public:
+       void calculateArea()
+       {   int s=50; cout<<s/5;  }
+};
+
+class Circle : public Shape
+{
+    public:
+       void calculateArea()
+       { int s=30; cout<<s/2; }
+};
+
+int main()
+{
+    Square s;
+    Circle c;
+
+     s.calculateArea();
+   c.calculateArea();
+
+    return 0;
+}
