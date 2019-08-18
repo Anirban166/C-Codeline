5bd0f14ff0590dcd1de67f5cf4fe414b01a50a75
Anirban Chetia <bloodraven166@gmail.com>
2019-08-18 22:39:52 +0530

    Create Print_SLL.c

---
+++ Print_SLL.c
@@ -0,0 +1,34 @@
+//Structure:
+{ int data;
+  struct x*next;
+};
+typedef struct x node;
+node *root=NULL;
+
+//Iterative Function to display elements:
+func()
+{ 
+  if(root==NULL)
+  printf("Link list is empty");
+  else
+  { node *t=root;
+    while(t!=NULL)
+    { printf("%d->",t->data);
+      t=t->next;
+    } printf("NULL"); //at the end of the list. Eg display: "3->4->NULL"
+  }
+    //OR use for loop:
+    //for(t=root;t!=NULL;t=t->next)
+    //printf("%d->",t->data);
+    //printf("NULL");
+}
+
+//Recursive function to display the elements of a SLL:
+func(node *t)
+{ if(root!=NULL) //if(root)
+  { printf("%d->",t->data);
+    func(t->next);
+  }
+} 
+
+
