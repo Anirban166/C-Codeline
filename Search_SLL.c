e2f6b2f1f48c53b0681074436633dad4b5f6d827
Anirban Chetia <bloodraven166@gmail.com>
2019-08-18 21:53:57 +0530

    Create Search_SLL.c

---
+++ Search_SLL.c
@@ -0,0 +1,15 @@
+func()
+{ node *t=root;
+  int c=0, x;
+  while(t!=NULL)
+ { if(t->data==x)
+   count++;
+   // printf("Found at %d position",c+1);
+   t=t->next;
+ }
+ if(c!=0)
+ printf("Element found. \n");
+ else
+ printf("Not found. \n");
+}
+
