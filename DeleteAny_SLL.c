dd300fd98d97f68f34de17b00ade35b0675857a6
Anirban Chetia <bloodraven166@gmail.com>
2019-08-18 22:29:59 +0530

    Delete a node from any position in a SLL.

---
+++ DeleteAny_SLL.c
@@ -0,0 +1,18 @@
+//Structure:
+{ int data;
+  struct x*next;
+};
+typedef struct x node;
+node *root=NULL;
+
+//Function to delete a node from any position (given as input) from a SLL:
+func()
+{ node *t,*temp;
+  int count=0,n;
+  scanf("%d",&n);
+  //No body for loop with break status true when condition not satisfied.
+  for(t=root;(count++!)=n && t->next!=NULL;t=t->next);
+  temp=t->next;
+  t->next=t->next->next;
+  free(temp);
+}
