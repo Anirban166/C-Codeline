15905c72a919bce297606e3988ae2c6393a35c85
Anirban Chetia <bloodraven166@gmail.com>
2019-08-20 11:44:56 +0530

    Stack using Linked List.

---
+++ Linked Lists | C/Stack_LinkList.c
@@ -0,0 +1,46 @@
+//Structure:
+struct x
+{ int data;
+  struct x*next;
+}; typedef struct x node;
+node *top=NULL;
+
+//Push function:
+push()
+{ int n;
+  node *N;
+  N=(node*)malloc(sizeof(node));
+  printf("Enter node data\n");
+  scanf("%d",&n);
+  N->data=n;
+  N->next=NULL;
+  if(top==NULL)
+  { top=N; }
+  else
+  { N->next=top; top=N;
+  }
+}
+
+//Pop function:
+pop()
+{ if(top=NULL)
+   printf("Empty Stack");
+   else
+   { node *t=top;
+     top=top->next;
+     free(t);
+   }
+}
+
+//Display function:
+func()
+{ if(top=NULL)
+  printf("Empty Stack");
+  else
+  { node *t;
+    for(t=top;t!=NULL;t=t->next)
+    printf("%d->",t->data);
+    printf("NULL");
+  }
+}
+
