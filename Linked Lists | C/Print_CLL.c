2005f74933b7b0b340cd69c32ed71ef1c95e7a38
Anirban Chetia <bloodraven166@gmail.com>
2019-08-20 11:58:56 +0530

    Added recursive display functions.

--- Linked Lists | C/Print_CLL.c
+++ Linked Lists | C/Print_CLL.c
@@ -16,3 +16,21 @@ func()
   printf("%d",t->data); //to print root->data.
 }
            
+//Recursive function to display the elements in a CLL:
+func(node *t)
+{ if(root!=NULL) //if(root)
+  { printf("%d->",t->data);
+    func(t->next);
+  }
+ // printf("%d",t->data); //to print root->data.
+} 
+
+//Recursive function to display the elements in reverse order in a CLL:
+func(node *t)
+{ if(root!=NULL) //if(root)
+  { func(t->next);
+    printf("%d->",t->data);
+  } 
+ //  printf("%d",t->data); //to print root->data.
+} 
+ 
