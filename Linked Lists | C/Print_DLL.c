d1705d23b620f94ad1d51c7e790825d758026f1c
Anirban Chetia <bloodraven166@gmail.com>
2019-08-20 11:39:00 +0530

    Added recursive functions.

--- Linked Lists | C/Print_DLL.c
+++ Linked Lists | C/Print_DLL.c
@@ -15,3 +15,20 @@ func()
   }
   printf("NULL");
 }
+
+//Recursive function to display the elements of a DLL:
+func(node *t)
+{ if(root!=NULL) //if(root)
+  { printf("%d<->",t->data);
+    func(t->next);
+  }
+} 
+
+//Recursive function to display the elements in reverse order of a DLL:
+func(node *t)
+{ if(root!=NULL) //if(root)
+  { func(t->next);
+    printf("%d<->",t->data);
+  }
+} 
+ 
