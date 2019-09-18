2f91efe73c76cd62cba3ae28ad55a11163e83e3e
Anirban Chetia <bloodraven166@gmail.com>
2019-09-18 17:31:09 +0530

    Series : x^2/2!+x^4/4!+x^6/6!+x^8/8!+...

---
+++ Series | C/SeriesA.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+
+long  fact(int  n) ;
+
+void  main()
+{
+    int  i, k, n ;
+    float  x, sum=0 ;
+    clrscr() ;
+    printf("Enter the number of terms: ") ;
+    scanf("%d", &n) ;
+    printf("Enter the value of x: ") ;
+    scanf("%f", &x) ;
+    /* i keeps track of term count , whereas k is used for calculation purpose */
+    for(i=1, k=0 ; i<=n ; i++, k=k+2)
+        {
+            if(i%2==1)
+                sum = sum + pow(x,k)/fact(k) ;
+            else
+                sum = sum - pow(x,k)/fact(k) ;
+        }
+    printf("Sum of the required series is %f", sum) ;
+    getch() ;
+}
+
+long fact(int  n)
+{
+    int  i ;
+    long f=1;
+    for(i=1 ; i<=n ; i++)
+        f=f*i ;
+    return  f ;
+}
